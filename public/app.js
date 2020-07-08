f55371fa4c36fdd595b110e8031bff64f344c84b
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-07-07 21:49:38 -0500

    Add articles to browser

--- public/app.js
+++
@@ -1,6 +0,0 @@
-// Grab article as a json object
-$.getJSON("/article", function(data) {
-  for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].teaser + "<br />" + '<img src= "' + data[i].image + '">' + "<br />" + "</p>");
-  }
-});
